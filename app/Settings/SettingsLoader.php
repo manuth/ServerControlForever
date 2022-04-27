458c58aef2c6bb8470a1766c13e5a407077c61e7
Manuel Thalmann <m@nuth.ch>
2022-04-27 02:08:29 +0200

    Add a stub class for loading settings

---
+++ app/Settings/SettingsLoader.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Gizmo\ServerControlForever\Settings;
+
+/**
+ * Provides the functionality to load settings.
+ */
+class SettingsLoader
+{
+    /**
+     * Initializes a new instance of the {@see SettingsLoader} class.
+     */
+    private function __construct()
+    {
+    }
+}
