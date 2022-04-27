406c20e8ac10657a825bab9328878555aa6dd069
Manuel Thalmann <m@nuth.ch>
2022-04-27 02:08:52 +0200

    Add a class representing a server

---
+++ app/Management/Server.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Gizmo\ServerControlForever\Management;
+
+/**
+ * Provides the functionality to control a TrackMania Server.
+ */
+class Server
+{
+    /**
+     * Initializes a new instance of the {@see Server} class.
+     */
+    public function __construct($config)
+    {
+    }
+}
