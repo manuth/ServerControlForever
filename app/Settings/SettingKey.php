c2a2f86193feb5ad5e2a9331391f8ad7912d2d79
Manuel Thalmann <m@nuth.ch>
2022-04-27 02:09:33 +0200

    Add a file for representing setting keys

---
+++ app/Settings/SettingKey.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Represents a setting key.
+ */
+enum SettingKey: string
+{
+    /**
+     * Indicates the setting for enabling abbreviated commands.
+     */
+    case AbbreviatedCommands = "abbreviatedCommands";
+}
