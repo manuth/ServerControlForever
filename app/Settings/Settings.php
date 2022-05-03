0a50dc9bdc982b8a5d78128f6b93c2b0244f99af
Manuel Thalmann <m@nuth.ch>
2022-05-03 17:43:02 +0200

    Fix broken setting manipulation

--- app/Settings/Settings.php
+++ app/Settings/Settings.php
@@ -60,17 +60,17 @@ class SettingsLoader
     /**
      * Gets the value of the setting located at the specified path.
      *
-     * @param string[] $path The path to the setting.
+     * @param SettingKey $path The path to the setting.
      */
-    protected function getValue(array ...$path)
+    protected function getValue(...$path)
     {
         $result = $this->getSettings();
 
         foreach ($path as $key)
         {
-            if (isset($result[$key]))
+            if (isset($result[$key->value]))
             {
-                $result = $result[$key];
+                $result = $result[$key->value];
             }
             else
             {
@@ -85,24 +85,117 @@ class SettingsLoader
      * Sets the value of the setting located at the specified path.
      *
      * @param mixed $value The value to set.
-     * @param string[] $path The path to the setting to set.
+     * @param SettingKey $path The path to the setting to set.
      */
-    protected function setValue($value, array ...$path): void
+    protected function setValue($value, ...$path): void
     {
         $container = $this->getSettings();
         $containerPath = collect($path);
+        /**
+         * @var SettingKey $lastKey
+         */
         $lastKey = $containerPath->pop();
+        $layers = collect();
 
         foreach ($containerPath as $key)
         {
-            if (!isset($container[$key]))
-            {
-                $container[$key] = [];
-            }
-
-            $container = $container[$key];
+            $layers->push($container[$key->value]);
+            $container = $container[$key->value];
         }
 
-        $container[$lastKey] = $value;
+        $layers->pop();
+        $container[$lastKey->value] = $value;
+
+        foreach ($layers as $layer)
+        {
+            /**
+             * @var SettingKey $key
+             */
+            $key = $containerPath->pop();
+            $layer[$key->value] = $container;
+            $container = $layer;
+        }
+    }
+
+    /**
+     * Gets a component of the server's address.
+     *
+     * @param int $component The component of the server's address to get.
+     * @return mixed The component of the server's address.
+     */
+    protected function getServerAddressComponent(int $component)
+    {
+        $path = collect([SettingKey::Server, SettingKey::ServerAddress]);
+        $result = $this->getValue(...$path);
+
+        if (is_string($result))
+        {
+            return parse_url($result, $component);
+        }
+        else
+        {
+            if ($component === PHP_URL_HOST)
+            {
+                $path->push(SettingKey::ServerHost);
+            }
+            else
+            {
+                $path->push(SettingKey::ServerPort);
+            }
+
+            return $this->getValue(...$path);
+        }
+    }
+
+    /**
+     * Sets the component of the server's address.
+     *
+     * @param int $component The component of the server's address to set.
+     * @param mixed $value The value to set.
+     */
+    protected function setServerAddressComponent(int $component, $value): void
+    {
+        $path = collect([SettingKey::Server, SettingKey::ServerAddress]);
+        $result = $this->getValue(...$path);
+        $hostPath = $path->concat([SettingKey::ServerHost]);
+        $portPath = $path->concat([SettingKey::ServerPort]);
+
+        if (!is_string($result))
+        {
+            $this->setValue($value, ...($component === PHP_URL_HOST ? $hostPath : $portPath));
+        }
+        else
+        {
+            /**
+             * @var int $existingComponent
+             */
+            $existingComponent;
+            /**
+             * @var string[] $existingPath
+             */
+            $existingPath;
+            /**
+             * @var string[] $newPath
+             */
+            $newPath;
+
+            if ($component === PHP_URL_HOST)
+            {
+                $existingComponent = PHP_URL_PORT;
+                $existingPath = $portPath;
+                $newPath = $hostPath;
+            }
+            else
+            {
+                $existingComponent = PHP_URL_HOST;
+                $existingPath = $hostPath;
+                $newPath = $portPath;
+            }
+
+            $existingValue = $this->getServerAddressComponent($existingComponent);
+            $this->setValue([], ...$path);
+            $this->setValue($existingValue, ...$existingPath);
+            $this->setValue($value, ...$newPath);
+        }
     }
 }
